c0677d7acbe85e3c87fbbf75d2cc203f5401e39c
agomeza21 <gomezandresa03@gmail.com>
2026-06-02 14:06:22 +0200

    validacion de config.txt terminada

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -9,8 +9,13 @@ def parse_config(file: str) -> dict[str, str]:
             for line in f:
                 stripped_line = line.strip()
                 if stripped_line and not stripped_line.startswith("#"):
-                    key, value = stripped_line.split("=", 1)
-                    content[key.strip()] = value.strip()
+                    if "=" in stripped_line:
+                        key, value = stripped_line.split("=", 1)
+                        content[key.strip().upper()] = value.strip()
+                    else:
+                        print(f"Error: Invalid configuration "
+                              f"format -> '{stripped_line}'")
+                        sys.exit(1)
     except FileNotFoundError:
         print(f"Error: configuration file '{file}' not found.")
         sys.exit(1)
@@ -18,19 +23,48 @@ def parse_config(file: str) -> dict[str, str]:
 
 
 def main() -> None:
-    if len(sys.argv) not in [2, 3]:
-        print("Usage: python3 a_maze_ing.py <config_file> [output_file.maze]")
+    if len(sys.argv) != 2:
+        print("Usage: python3 a_maze_ing.py <config_file>")
         sys.exit(1)
 
     content = parse_config(sys.argv[1])
-    width = int(content["WIDTH"])
-    height = int(content["HEIGHT"])
 
-    entry_x, entry_y = map(int, content["ENTRY"].split(","))
+    try:
+        width = int(content["WIDTH"])
+        height = int(content["HEIGHT"])
+        entry_x, entry_y = map(int, content["ENTRY"].split(","))
+    except (KeyError, ValueError):
+        print("Error: Invalid or missing numeric values in "
+              "configuration file.")
+        sys.exit(1)
+
+    if not (0 <= entry_x < width and 0 <= entry_y < height):
+        print("Error: ENTRY coordinates are out of maze bounds.")
+        sys.exit(1)
+
     entry = (entry_y, entry_x)
+
+    output_filename = content.get("OUTPUT_FILE")
+    key, value = output_filename.split(".", -1)
+    if value != "txt":
+        print("Error: outputfile is not a '.txt'")
+        sys.exit(1)
+    if not output_filename:
+        output_filename = "maze.txt"
 
     generator = MazeGenerator(width, height, entry)
     generator.generate()
+
+    try:
+        with open(output_filename, "w") as f:
+            f.write(generator.format_as_hex())
+        print("Maze generated correctly!")
+        print(f"File saved as '{output_filename}'.")
+    except Exception as e:
+        print(f"Error saving file: {e}")
+        sys.exit(1)
+
+    print("\n--- GRAPHIC RENDER ---")
     print(generator.render())
 
 
